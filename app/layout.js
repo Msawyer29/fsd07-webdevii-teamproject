a7f964073da9b14a91b058070038d46c63d047c2
Msawyer29 <119361495+Msawyer29@users.noreply.github.com>
2023-06-09 16:19:56 -0400

    first commit - project structure

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import '@styles/globals.css';
+import { Html } from 'next/document';
+
+export const metadata = {
+    title: "The Hive",
+    description: 'Crowdfunding Platform for Creative Projects'
+}
+
+const layout = () => {
+  return (
+    <html lang="en">
+        <body>
+            <div></div>
+        </body>
+
+    </html>
+  )
+}
+
+export default layout
