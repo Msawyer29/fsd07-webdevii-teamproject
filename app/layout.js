ec46a4700742778c7558014caacfda5070283fb3
Arjun Dhakal <arjun.nbsm@yahoo.com>
2023-06-10 12:28:56 -0400

    bootstrap added, and tailwind uninstalled

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,29 @@
-import '@styles/globals.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
 export const metadata = {
-    title: "The Hive",
-    description: 'Crowdfunding Platform for Creative Projects'
-}
+  title: "The Hive",
+  description: "Crowdfunding Platform for Creative Projects",
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+      <head>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-            <main className="app">
-                {children}
-            </main>
-        </body>
-
+        <main className="app">{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
