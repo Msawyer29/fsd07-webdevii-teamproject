451c86d812949dd2534b105ecebedbf815d01f43
Arjun Dhakal <arjun.nbsm@yahoo.com>
2023-06-14 12:30:51 -0400

    getAllProject.js add, projectRow.js add, addProject.js modified

--- app/page.js
+++ app/page.js
@@ -12,17 +12,19 @@ import Successful from "@components/successful";
 import Discover from "@components/discover";
 import AddProject from "../components/addProject";
 import FileUploader from "@components/fileUpload";
+import GetAllProject from "@components/getAllProject";
 
 const Home = () => (
   <div className="container px-5">
     <Categories />
+    <GetAllProject />
     <Featured />
     <Successful />
     <Discover />
     <FileUploader />
     <AddProject />
     <SignUp />
-    <GetUserByEmail />
+    {/* <GetUserByEmail /> */}
 
     {/* DELETE BELOW WHEN READY */}
 
---
+++ components/projectRow.js
@@ -0,0 +1,77 @@
+"use client";
+//import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
+import { firebaseStorage } from "../firebase/config";
+//import React, { useState, useRef } from "react";
+//import { getStorage, ref } from "firebase/storage";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+const ProjectRow = ({ project }) => {
+  let createrID = project.createrId;
+  let projectID = project.id;
+  let imageUrl = project.image;
+  let projectDesc = project.description.substring(0, 240) + " .....";
+  return (
+    <div className="mb-5">
+      <h6 className="egg text-uppercase mt-5 mb-3">featured projects</h6>
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="row d-flex">
+              <div className="col-md-8 px-3">
+                <img src={imageUrl} className="img-fluid" alt="..." />
+              </div>
+              <div className="col-md-4 px-3">
+                <h2 className="egg projectTitle text-capitalize">
+                  {project.title}
+                </h2>
+
+                <p className="green projectDescription">{projectDesc}</p>
+                <a
+                  href="#"
+                  className="green"
+                  // className="egg fst-italic projectAuthor"
+                  onClick={(e) => getCreaterProfile({ createrID }, e)}
+                >
+                  Creater Profile &#8594;
+                </a>
+                <h4>
+                  <a
+                    className="green"
+                    href="#"
+                    onClick={(e) => getCreaterProfile({ projectID }, e)}
+                  >
+                    read more &#8594;
+                  </a>
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectRow;
