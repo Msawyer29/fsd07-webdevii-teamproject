1e5b8263cef7d0bcb0dc97ce4db687e6f7373836
Msawyer29 <119361495+Msawyer29@users.noreply.github.com>
2023-06-14 14:32:54 -0400

    button disable, form refresh, success/fail message

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -1,29 +1,40 @@
 import React, { useState } from "react";
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ onPaymentSuccess, onPaymentError }) => {  // Destructure onPaymentSuccess & onPaymentError from props (passed as a prop form stripeModal component)
+  // useStripe and useElements are hooks provided by Stripe to access the stripe object and card element respectively
   const stripe = useStripe();
   const elements = useElements();
+
+  // processing state is used to control the disabling of the "Pay" button. If set to true, the button will be disabled.
   const [processing, setProcessing] = useState(false);
+
+  // paymentCompleted state variable for payment completion, after successful payment the Pay button is disabled
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!stripe || !elements) {
+    // If stripe or elements haven't loaded yet, stop the execution of the handleSubmit function
+    if (!stripe || !elements || paymentCompleted) {
       return;
     }
-    setProcessing(true); // disables the pay button after submission
     
+    // Once the form is submitted, we set processing to true to disable the button while the payment is processed
+    setProcessing(true);
+    
+    // Fetch the client secret from our backend to use in the payment process
     const response = await fetch("/api/create-payment-intent", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ amount: 1000 }), // Amount is in cents
+      body: JSON.stringify({ amount: 1000 }), // Amount is in cents when using Stripe API
     });
 
     const clientSecret = await response.text();
 
+    // Confirm the card payment using stripe's confirmCardPayment method
     const result = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements.getElement(CardElement),
@@ -33,21 +44,27 @@ const CheckoutForm = () => {
       },
     });
 
+    // Check if payment succeeded or failed.
     if (result.error) {
       console.log(result.error.message);
+      onPaymentError("Payment declined. Please consult your credit card provider for more details."); // call the passed in function from stripeModal when payment fails
+      setProcessing(false); // enables the pay button again if there was an error
     } else {
       if (result.paymentIntent.status === "succeeded") {
-        console.log("Payment processed successfully!");
+        console.log("Payment processed successfully");
+        setPaymentCompleted(true); // setting the paymentCompleted to true disables the Pay button after successful payment
+        onPaymentSuccess(); // call the passed in function from stripeModal when payment is successful
       }
+      setProcessing(false); // enables the pay button again after successful processing
     }
   };
-  
 
+  // Render the form and disable the Pay button if either stripe has not loaded yet or we are in the middle of processing a payment and when payment is successful
   return (
     <div id="form">
     <form onSubmit={handleSubmit}>
       <CardElement />
-      <button className="btn btn-primary mt-3 " type="submit" disabled={!stripe}>
+      <button className="btn btn-primary mt-3 " type="submit" disabled={!stripe || processing || paymentCompleted}>
         Pay
       </button>
     </form>
--- components/stripeModal.js
+++ components/stripeModal.js
@@ -1,44 +1,83 @@
-"use client"
+"use client";
+import React, { useState, useEffect, useRef } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "@components/CheckoutForm";
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe("pk_test_51MsdkuKcBJEP5unczDs6Q8CfWFl7rGELmQBhDbj9PzXAHfwLcF1xXkYs1FwzNdEhA1xsS59QqIbWzvXBZS7TZPC700sreRX3uA");
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid recreating the `Stripe` object on every render
+const stripePromise = loadStripe(
+  "pk_test_51MsdkuKcBJEP5unczDs6Q8CfWFl7rGELmQBhDbj9PzXAHfwLcF1xXkYs1FwzNdEhA1xsS59QqIbWzvXBZS7TZPC700sreRX3uA"
+);
 
 const StripeModal = () => {
+  const [modalKey, setModalKey] = useState(0); // initialize modalKey as 0
+  const [paymentSuccess, setPaymentSuccess] = useState(false); // new state for payment success
+  const [paymentError, setPaymentError] = useState(null); // new state for payment error
+  const modalRef = useRef(); // ref (modalRef) is created and attached to the modal's root div, see line 36
+
+  useEffect(() => {
+    const modalElement = modalRef.current;
+
+    if (!modalElement) {
+      return;
+    }
+
+    const handleHidden = () => {
+      setModalKey(modalKey + 1); // increase modalKey by 1, this refreshes ChekoutForm by creating a new instance of it when modal is closed/hidden
+      setPaymentSuccess(false); // reset paymentSuccess when the modal is closed
+    };
+    // event listener for the "hidden.bs.modal" event is attached to the modal element
+    // this ensures that the handleHidden function (which increments modalKey) is called every time the modal is closed
+    modalElement.addEventListener('hidden.bs.modal', handleHidden);
+
+    // Cleanup function to remove event listener
+    return () => {
+      modalElement.removeEventListener('hidden.bs.modal', handleHidden);
+    };
+  }, [modalKey]);
+
   return (
     <div
-            className="modal fade"
-            id="stripePay"
-            tabIndex="-1"
-            aria-labelledby="stripePayLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog modal-dialog-centered">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5 dGreen" id="stripePayLabel">
-                  Dear [Donor's Name],
-                  </h1>
-            
-                  <button
-                    type="button"
-                    className="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <p className="dGreen px-3 mt-3">Thank you for your generous donation towards our art project. Your support not only empowers independent artists but also helps bring our creative vision to life.</p>
-                <div className="modal-body px-5">
-                  <Elements stripe={stripePromise}>
-                    <CheckoutForm />
-                  </Elements>
-                </div>
-              </div>
-            </div>
-          </div>
-  )
-}
+      ref={modalRef}
+      className="modal fade"
+      id="stripePay"
+      tabIndex="-1"
+      aria-labelledby="stripePayLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5 dGreen" id="stripePayLabel">
+              Dear [Donor's Name],
+            </h1>
 
-export default StripeModal
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <p className="dGreen px-3 mt-3">
+            Thank you for your generous donation towards our project. Your
+            support not only empowers independent artists but also helps bring
+            our creative vision to life.
+          </p>
+          {paymentSuccess && <div className="alert alert-success mt-3">Thank you! Your payment was completed successfully.</div>} {/* display success alert when paymentSuccess is true */}
+          {paymentError && <div className="alert alert-danger mt-3">{paymentError}</div>} {/* display error alert when paymentError is set */}
+          <div className="modal-body px-5">
+            <Elements stripe={stripePromise} key={modalKey}> {/* use modalKey as key prop, this forces React to create a new instance of the CheckoutForm component each time the key changes*/}
+            <CheckoutForm 
+              onPaymentSuccess={() => setPaymentSuccess(true)}
+              onPaymentError={(error) => setPaymentError(error)} // pass functions, set setPaymentSuccess to true & to set paymentError
+            /> 
+            </Elements>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StripeModal;
