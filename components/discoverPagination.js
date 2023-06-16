ef398d69737502137024912091c71fcefff9ad38
Msawyer29 <119361495+Msawyer29@users.noreply.github.com>
2023-06-16 09:20:42 -0400

    discoverPagination fixed

--- components/discoverPagination.js
+++ components/discoverPagination.js
@@ -14,18 +14,17 @@ import {
 // define the functional component DiscoverPagination
 function DiscoverPagination() {
   // initialize state variables for storing the projects, the loading state and the index of the first project to display
-  const [projects, setProjects] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [firstProjIndex, setFirstProjIndex] = useState(0)
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [firstProjIndex, setFirstProjIndex] = useState(0);
 
   // get the firestore database instance from the firebase app
-  const db = getFirestore(firebase_app)
+  const db = getFirestore(firebase_app);
 
-  let projectsArray = []
-
+  let projectsArray = [];
   // inside the useEffect hook, an observer is attached to the 'projects' collection in firestore
   useEffect(() => {
-    const q = query(collection(db, "projects"), orderBy("startDate", "desc"))
+    const q = query(collection(db, "projects"), orderBy("startDate", "desc"));
     const projectssDetails = onSnapshot(q, (projSnaps) => {
       // the forEach loop iterates through the snapshot of the data returned by the observer
       projSnaps.docs.forEach((pSnap) => {
@@ -33,16 +32,16 @@ function DiscoverPagination() {
         projectsArray.push({
           ...pSnap.data(),
           id: pSnap.id,
-        })
-      })
+        });
+      });
       // update the state variables with the new data
-      setProjects(projectsArray)
-      setLoading(false)
+      setProjects(projectsArray);
+      setLoading(false);
 
       // reset the projectsArray for the next potential render
-      projectsArray = []
-    })
-  }, [])
+      projectsArray = []; //reset projectsArray
+    });
+  }, []);
 
   // while the data is still loading, display a loading spinner
   if (loading) {
@@ -52,25 +51,26 @@ function DiscoverPagination() {
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
-    )
+    );
   }
 
   // calculate the last project index to display and slice the projects array accordingly
-  let lastProjectIndex = firstProjIndex + 4
-  const projectToDisplay = projects.slice(firstProjIndex, lastProjectIndex)
-
-  // define the click handlers for the pagination buttons
+  let lastProjectIndex = firstProjIndex + 4;
+  const projectToDisplay = projects.slice(firstProjIndex, lastProjectIndex); //need to pass this data
   const leftArrowClick = () => {
     if (firstProjIndex > 0) {
-      setFirstProjIndex(firstProjIndex - 1)
+      setFirstProjIndex(firstProjIndex - 1);
+      console.log(firstProjIndex);
     }
-  }
+  };
 
+  // define the click handlers for the pagination buttons
   const rightArrowClick = () => {
     if (firstProjIndex < projects.length - 3) {
-      setFirstProjIndex(firstProjIndex + 1)
+      setFirstProjIndex(firstProjIndex + 1);
+      console.log(firstProjIndex);
     }
-  }
+  };
 
   // return the JSX for rendering the component
   return (
@@ -80,13 +80,24 @@ function DiscoverPagination() {
       <hr className="green" />
       <div className="row d-flex my-4 py-5">
         {projectToDisplay.map((p) => (
-          // map through the projects to display and render each one
           <div key={p.id} className="col-md-3">
-            // truncated for brevity
+            <div className="card">
+              <img src={p.image} className="img-fluid card-img-top" alt="..." />
+              <div className="d-flex justify-content-center">
+                <div className="card-body">
+                  <h5 className="card-title titleLimit">{p.title}</h5>
+                  <p className="card-text shortDescription ">{p.description}</p>
+                  <h6 className="mt-2">
+                    <a className="dGreen " href={"/single-project/" + p.id}>
+                      read more &#8594;
+                    </a>
+                  </h6>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
         <div className="d-flex justify-content-between pagBtnDiv">
-          // render the pagination buttons
           <div>
             <button
               className={firstProjIndex == 0 ? "hideMe" : ""}
@@ -106,8 +117,7 @@ function DiscoverPagination() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-// export the DiscoverPagination component for use in other files
-export default DiscoverPagination
+export default DiscoverPagination;
